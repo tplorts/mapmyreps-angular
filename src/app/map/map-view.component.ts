dae18a2860cde9b67ba711854a42e0ee281de5a6
Ted Lorts <tplorts@gmail.com>
2017-12-13 21:26:00 -0800

    cleaned up map-view controller code and now including us-atlas instead of requesting topological data on page load

--- src/app/map/map-view.component.ts
+++ src/app/map/map-view.component.ts
@@ -1,45 +1,22 @@
-import {
-  Component,
-  OnInit,
-  ElementRef,
-  ViewChild,
-  HostListener,
-} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Response, Http } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
-import {
-  map,
-  // delay
-} from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 
 import { toNumber, isEqual } from 'lodash';
 
-import { select, selectAll, Selection } from 'd3-selection';
-import {
-  GeoPath,
-  geoPath,
-  geoAlbersUsa,
-} from 'd3-geo';
-import { json } from 'd3-request';
+import { GeoPath, geoPath } from 'd3-geo';
 import { feature, mesh } from 'topojson';
 
 import { Logger } from '../core/logger.service';
-import { StateNames, StateAbbreviations } from './usa-states';
 import { LegislatorsService, Senator, Representative } from '../data/legislators.service';
-// import { DataStateService } from '../data/data-state.service';
+
+import { StateNames, StateAbbreviations } from './usa-states';
+import * as UsaTopology from 'us-atlas/us/10m.json';
 
 
 
 const log = new Logger('MapViewComponent');
-
-
-
-type SVGSelection = Selection<SVGElement, {}, null, undefined>;
-
-// interface IStateGeometry {
-//   pathData: string;
-//   feature: any;
-// }
 
 interface IGeoCodeResults {
   status: string;
@@ -53,83 +30,46 @@ interface IGeoCodeResults {
 })
 export class MapViewComponent implements OnInit {
 
-  // @ViewChild('svg') _svg: SVGElement;
   private _options: { width: number, height: number } = { width: 960, height: 600 };
-
-  svg: SVGSelection;
-  path: GeoPath<any, any>;
-  usaStatesGeoData: any;
-  isLoading: boolean;
-  statesGroup: SVGSelection;
-  dataPromise: Promise<any>;
-  bordersData: string;
-  // stateGeometries: IStateGeometry[];
-  stateFeatures: any[];
-  selectedState: any;
   private shuffleIntervalId: number;
+
+  public stateBordersPathData: string;
+  public stateFeatures: any[];
+  public selectedState: any;
 
   private static isOfState(state: string) {
     return (x: any) => state === x.state;
   }
 
   constructor(
-    private myElement: ElementRef,
-    // private dataState: DataStateService, // maybe later
     private legislators: LegislatorsService,
     private http: Http,
   ) {
     this.selectedState = null;
     this.shuffleIntervalId = null;
-    this.isLoading = true;
-
-    this.dataPromise = new Promise<any>((resolve, reject) => {
-      json('https://d3js.org/us-10m.v1.json', (error, data: any) => {
-        if (error) {
-          reject(error);
-          throw error;
-        }
-        this.usaStatesGeoData = data;
-        // this.isLoading = false;
-        resolve();
-      });
-    });
   }
 
   async ngOnInit() {
-    // this.matchWindowSize();
-
     const unequal = (a: any, b: any) => a !== b;
-
-    const svgElement = this.myElement.nativeElement.querySelector('svg');
-    this.svg = select(svgElement);
-    this.statesGroup = this.svg.select('g.states');
-
-    // const projection = geoAlbersUsa()
-    //   .scale(1070)
-    //   .translate([this.width / 2, this.height / 2]);
-
-    this.path = geoPath(/*projection*/);
-
-    await this.dataPromise;
-    this.bordersData = this.path(mesh(this.usaStatesGeoData, this.usaStatesGeoData.objects.states, unequal));
-    const features = feature(this.usaStatesGeoData, this.usaStatesGeoData.objects.states).features;
+    const path: GeoPath<any, any> = geoPath();
+    const StateGeometries = UsaTopology.objects.states;
+    const borders = path(mesh(UsaTopology, StateGeometries, unequal));
+    const features = feature(UsaTopology, StateGeometries).features;
     for (const f of features) {
-      f.key = f.id;
-      f.id = toNumber(f.key) - 1;
+      f.identifier = toNumber(f.id);
     }
-    const byId = (a: any, b: any) => a.id - b.id;
-    features.sort(byId);
+    const byIdentifier = (a: any, b: any) => a.identifier - b.identifier;
+    features.sort(byIdentifier);
 
     let index = 0;
     for (const f of features) {
       f.name = StateNames[index++];
       f.abbreviation = StateAbbreviations[f.name];
-      f.pathData = this.path(f);
+      f.pathData = path(f);
     }
 
     this.stateFeatures = features;
-
-    this.isLoading = false;
+    this.stateBordersPathData = borders;
   }
 
   public selectForLocation(): void {
@@ -193,6 +133,9 @@ export class MapViewComponent implements OnInit {
   }
 
   public shuffle(): void {
+    if (this.isShuffling()) {
+      return;
+    }
     this.shuffleIntervalId = window.setInterval(() => this.selectRandomState(), 150);
     const time = Math.round(Math.random() * 5e3);
     const stop = () => {
@@ -236,16 +179,6 @@ export class MapViewComponent implements OnInit {
     const state = this.selectedState;
     return state && `${state.name} (${state.abbreviation})`;
   }
-
-  // @HostListener('window:resize', ['$event'])
-  // onResize(event: any) {
-  //   this.matchWindowSize();
-  // }
-
-  // private matchWindowSize(): void {
-  //   this._options.width = window.innerWidth;
-  //   this._options.height = window.innerHeight;
-  // }
 
   public get width(): number {
     return this._options.width;
