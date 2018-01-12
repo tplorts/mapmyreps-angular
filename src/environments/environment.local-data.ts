7468a92ea2877907abf38e7a884f6e41d8a18213
Ted Lorts <tplorts@gmail.com>
2018-01-12 10:14:12 -0800

    new environment for locally served static data

---
+++ src/environments/environment.local-data.ts
@@ -0,0 +1,7 @@
+import { environment as defaultEnvironment } from './environment.default';
+
+export const environment: any = {
+  ...defaultEnvironment,
+  serverUrl: 'http://localhost:3000/api',
+  staticDataUrl: 'http://localhost:5000',
+};
