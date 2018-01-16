6af042eb1116601fbc02a95636ccc2804e153df0
Ted Lorts <tplorts@gmail.com>
2018-01-15 16:22:40 -0800

    added the dev data bucket

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -2,4 +2,5 @@ import { environment as defaultEnvironment } from './environment.default';
 
 export const environment: any = {
   ...defaultEnvironment,
+  staticDataUrl: 'http://dev-data.mapmyreps.us',
 };
