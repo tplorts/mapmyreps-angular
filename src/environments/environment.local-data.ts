10a350b6c3dab77b080b49a3b947072d33b28fd4
Ted Lorts <tplorts@gmail.com>
2018-01-26 13:25:56 -0500

    local data environment for running app without network connection

---
+++ src/environments/environment.local-data.ts
@@ -0,0 +1,6 @@
+import { environment as defaultEnvironment } from './environment.default';
+
+export const environment: any = {
+  ...defaultEnvironment,
+  staticDataUrl: 'http://localhost:5000',
+};
